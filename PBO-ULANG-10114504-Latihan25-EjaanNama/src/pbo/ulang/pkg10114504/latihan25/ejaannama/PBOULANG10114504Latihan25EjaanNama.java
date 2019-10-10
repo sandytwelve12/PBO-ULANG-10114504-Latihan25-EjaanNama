6473590f310eda9a848cc28b75d66d54ddbd912b
Sandy Akbar <55833676+sandytwelve12@users.noreply.github.com>
2019-10-10 09:26:38 -0700

    Add files via upload

---
+++ PBO-ULANG-10114504-Latihan25-EjaanNama/src/pbo/ulang/pkg10114504/latihan25/ejaannama/PBOULANG10114504Latihan25EjaanNama.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10114504.latihan25.ejaannama;
+
+
+/**
+ *
+ * @author Andri
+ */
+import java.util.Scanner;
+public class PBOULANG10114504Latihan25EjaanNama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //inisialisasi
+        Scanner input = new Scanner(System.in);
+        
+        //input nama
+        for (char i = 'A'; i <= 'Z'; i++){     
+            System.out.println("Masukan nama depan anda untuk di eja:");i = (char) input.nextInt();
+            
+        //
+        
+     
+    }
+    
+    }
